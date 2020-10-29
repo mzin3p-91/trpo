e7bdc098ae0ebc9d020df3f3d33c8f42a32f19ea
Fitz <Fitz@example.com>
2020-10-29 13:44:18 +0700

    main

---
+++ mylib.hpp
@@ -0,0 +1,18 @@
+#ifndef MY_LIBRARY_H_INCLUDED
+#define MY_LIBRARY_H_INCLUDED
+
+#include <cstdlib>
+
+
+int read_number();
+
+
+void factorize(int number, int* Divisor, int* Divisor_top);
+
+
+void print_number(int number);
+
+
+void print_array(int A[], size_t A_size);
+
+#endif 
